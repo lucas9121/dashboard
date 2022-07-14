3e6fcfcbc9f943b5f0f7185746b13f1b6fca1956
Lucas <lucas2carlos@gmail.com>
2022-07-14 14:34:55 -0400

    building Home page

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,10 @@
+import FeaturedInfo from '../../components/featuredInfo/FeaturedInfo'
+import './Home.css'
+
+export default function Home(props){
+    return(
+        <div className="Home">
+            <FeaturedInfo />
+        </div>
+    )
+}
