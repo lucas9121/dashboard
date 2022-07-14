d1a679934a0e148911226d53c5f7517d61205de7
Lucas <lucas2carlos@gmail.com>
2022-07-13 23:24:57 -0400

    fixing icons

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,3 @@
-import {BsBell} from 'react-icons/bs'
 import {RiSettings5Fill} from 'react-icons/ri'
 import {MdLanguage, MdNotificationsNone} from 'react-icons/md'
 export default function Header(props) {
@@ -9,15 +8,15 @@ export default function Header(props) {
                     <span className="logo">Lucas Carlos</span>
                 </div>
                 <div className="top-right">
-                    <div className="Icons">
+                    <div className="Header-icons">
                         <MdNotificationsNone style={{fontSize: '2.2rem'}}/>
                         <span className="iconBadge">2</span>
                     </div>
-                    <div className="Icons">
+                    <div className="Header-icons">
                         <MdLanguage style={{fontSize: '2.1rem'}}/>
                         <span className="iconBadge">2</span>
                     </div>
-                    <div className="Icons">
+                    <div className="Header-icons">
                         <RiSettings5Fill style={{fontSize: '2.3rem'}} />
                     </div>
                 </div>
