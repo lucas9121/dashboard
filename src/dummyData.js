8576c54c16bb1b1bdc47a6c860034abca06c9fe7
Lucas <lucas2carlos@gmail.com>
2022-07-15 18:45:08 -0400

    making fake chart data

---
+++ src/dummyData.js
@@ -0,0 +1,52 @@
+const userData = [
+    {
+      name: 'Jan',
+      ActiveUser: 4000,
+    },
+    {
+      name: 'Feb',
+      ActiveUser: 3000,
+    },
+    {
+      name: 'Mar',
+      ActiveUser: 2000,
+    },
+    {
+      name: 'Apr',
+      ActiveUser: 2780,
+    },
+    {
+      name: 'May',
+      ActiveUser: 1890,
+    },
+    {
+      name: 'Jun',
+      ActiveUser: 2390,
+    },
+    {
+      name: 'Jul',
+      ActiveUser: 3490,
+    },
+    {
+      name: 'Aug',
+      ActiveUser: 2390,
+    },
+    {
+      name: 'Sep',
+      ActiveUser: 1490,
+    },
+    {
+        name: 'Oct',
+        ActiveUser: 5490,
+    },
+    {
+        name: 'Nov',
+        ActiveUser: 4500,
+    },
+    {
+        name: 'Dec',
+        ActiveUser: 1100,
+    },
+  ];
+
+  export default userData
