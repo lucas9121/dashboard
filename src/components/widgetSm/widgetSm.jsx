1b5bdefa00ca9fc6f3fc5f71112fee5415b14c44
Lucas <lucas2carlos@gmail.com>
2022-07-17 22:42:23 -0400

    building small widget

--- src/components/widgetSm/widgetSm.jsx
+++ src/components/widgetSm/widgetSm.jsx
@@ -1,9 +1,23 @@
 import './widgetSm.css'
+import {MdVisibility} from 'react-icons/md'
 
 export default function WidgetSm(props){
     return(
         <div className='widgetSm'>
-            small
+            <span className="widgetSmTitle">New Join Members</span>
+            <ul className="widgetSmList">
+                <li className="widgetSmListItem">
+                    <img src="https://img.pokemondb.net/artwork/eevee.jpg" alt="" />
+                    <div className="widgetSmUser">
+                        <span className="widgetSmUsername">Ash Ketchum</span>
+                        <span className="widgetSmUserTitle">Software Engineer</span>
+                    </div>
+                    <button className="widgetSmButton">
+                        <MdVisibility />
+                        Display
+                    </button>
+                </li>
+            </ul>
         </div>
     )
 }
