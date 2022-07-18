f17c0760c7cec79ca44507b8bd714c917a822684
Lucas <lucas2carlos@gmail.com>
2022-07-18 16:13:25 -0400

    building small widget

--- src/components/widgetSm/widgetSm.jsx
+++ src/components/widgetSm/widgetSm.jsx
@@ -17,6 +17,50 @@ export default function WidgetSm(props){
                         Display
                     </button>
                 </li>
+                <li className="widgetSmListItem">
+                    <img src="https://img.pokemondb.net/artwork/eevee.jpg" alt="" />
+                    <div className="widgetSmUser">
+                        <span className="widgetSmUsername">Ash Ketchum</span>
+                        <span className="widgetSmUserTitle">Software Engineer</span>
+                    </div>
+                    <button className="widgetSmButton">
+                        <MdVisibility />
+                        Display
+                    </button>
+                </li>
+                <li className="widgetSmListItem">
+                    <img src="https://img.pokemondb.net/artwork/eevee.jpg" alt="" />
+                    <div className="widgetSmUser">
+                        <span className="widgetSmUsername">Ash Ketchum</span>
+                        <span className="widgetSmUserTitle">Software Engineer</span>
+                    </div>
+                    <button className="widgetSmButton">
+                        <MdVisibility />
+                        Display
+                    </button>
+                </li>
+                <li className="widgetSmListItem">
+                    <img src="https://img.pokemondb.net/artwork/eevee.jpg" alt="" />
+                    <div className="widgetSmUser">
+                        <span className="widgetSmUsername">Ash Ketchum</span>
+                        <span className="widgetSmUserTitle">Software Engineer</span>
+                    </div>
+                    <button className="widgetSmButton">
+                        <MdVisibility />
+                        Display
+                    </button>
+                </li>
+                <li className="widgetSmListItem">
+                    <img src="https://img.pokemondb.net/artwork/eevee.jpg" alt="" />
+                    <div className="widgetSmUser">
+                        <span className="widgetSmUsername">Ash Ketchum</span>
+                        <span className="widgetSmUserTitle">Software Engineer</span>
+                    </div>
+                    <button className="widgetSmButton">
+                        <MdVisibility />
+                        Display
+                    </button>
+                </li>
             </ul>
         </div>
     )
