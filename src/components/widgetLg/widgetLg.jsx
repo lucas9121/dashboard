0ccb360b6ff66e9b176a32266e8d2ba926bbbd21
Lucas <lucas2carlos@gmail.com>
2022-07-19 15:20:23 -0400

    fixing table

--- src/components/widgetLg/widgetLg.jsx
+++ src/components/widgetLg/widgetLg.jsx
@@ -5,48 +5,52 @@ export default function WidgetLg(props){
         <div className='widgetLg'>
             <h3 className="widgetLgTitle">Latest Transactions</h3>
             <table className="widgetLgTable">
-                <tr className="widgetLgTr">
-                    <th className="widgetLgTh">Customer</th>
-                    <th className="widgetLgTh">Date</th>
-                    <th className="widgetLgTh">Amount</th>
-                    <th className="widgetLgTh">Status</th>
-                </tr>
-                <tr className="widgetLgTr">
-                    <td className="widgetLgUser">
-                        <img src="https://img.pokemondb.net/sprites/trainers/heartgold-soulsilver/clair.png" alt="" />
-                        <spam className="widgetLgName">Clair Blue</spam>
-                    </td>
-                    <td className="widgetLgDate">Jun 2, 2022</td>
-                    <td className="widgetLgAmount">$122.00</td>
-                    <td className="widgetLgStatus"><Button type="Approved" /></td>
-                </tr>
-                <tr className="widgetLgTr">
-                    <td className="widgetLgUser">
-                        <img src="https://img.pokemondb.net/sprites/trainers/heartgold-soulsilver/lance.png" alt="" />
-                        <spam className="widgetLgName">Lance Draco</spam>
-                    </td>
-                    <td className="widgetLgDate">Jun 2, 2022</td>
-                    <td className="widgetLgAmount">$122.00</td>
-                    <td className="widgetLgStatus"><Button type="Declined" /></td>
-                </tr>
-                <tr className="widgetLgTr">
-                    <td className="widgetLgUser">
-                        <img src="https://img.pokemondb.net/sprites/trainers/ruby-sapphire/drake.png" alt="" />
-                        <spam className="widgetLgName">Drake Ruby</spam>
-                    </td>
-                    <td className="widgetLgDate">Jun 2, 2022</td>
-                    <td className="widgetLgAmount">$122.00</td>
-                    <td className="widgetLgStatus"><Button type="Pending" /></td>
-                </tr>
-                <tr className="widgetLgTr">
-                    <td className="widgetLgUser">
-                        <img src="https://img.pokemondb.net/sprites/trainers/diamond-pearl/flint.png" alt="" />
-                        <spam className="widgetLgName">Flint Connor</spam>
-                    </td>
-                    <td className="widgetLgDate">Jun 2, 2022</td>
-                    <td className="widgetLgAmount">$122.00</td>
-                    <td className="widgetLgStatus"><Button type="Approved" /></td>
-                </tr>
+                <thead className="widgetLgTr">
+                    <tr>
+                        <th className="widgetLgTh">Customer</th>
+                        <th className="widgetLgTh">Date</th>
+                        <th className="widgetLgTh">Amount</th>
+                        <th className="widgetLgTh">Status</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr className="widgetLgTr">
+                        <td className="widgetLgUser">
+                            <img src="https://img.pokemondb.net/sprites/trainers/heartgold-soulsilver/clair.png" alt="user image" />
+                            <span className="widgetLgName">Clair Blue</span>
+                        </td>
+                        <td className="widgetLgDate">Jul 10, 2022</td>
+                        <td className="widgetLgAmount">$1,432.00</td>
+                        <td className="widgetLgStatus"><Button type="Approved" /></td>
+                    </tr>
+                    <tr className="widgetLgTr">
+                        <td className="widgetLgUser">
+                            <img src="https://img.pokemondb.net/sprites/trainers/heartgold-soulsilver/lance.png" alt="user image" />
+                            <span className="widgetLgName">Lance Draco</span>
+                        </td>
+                        <td className="widgetLgDate">Jul 10, 2022</td>
+                        <td className="widgetLgAmount">$103,528.00</td>
+                        <td className="widgetLgStatus"><Button type="Declined" /></td>
+                    </tr>
+                    <tr className="widgetLgTr">
+                        <td className="widgetLgUser">
+                            <img src="https://img.pokemondb.net/sprites/trainers/ruby-sapphire/drake.png" alt="user image" />
+                            <span className="widgetLgName">Drake Ruby</span>
+                        </td>
+                        <td className="widgetLgDate">Jul 10, 2022</td>
+                        <td className="widgetLgAmount">$824.00</td>
+                        <td className="widgetLgStatus"><Button type="Pending" /></td>
+                    </tr>
+                    <tr className="widgetLgTr">
+                        <td className="widgetLgUser">
+                            <img src="https://img.pokemondb.net/sprites/trainers/diamond-pearl/flint.png" alt="user image" />
+                            <span className="widgetLgName">Flint Connor</span>
+                        </td>
+                        <td className="widgetLgDate">Jul 10, 2022</td>
+                        <td className="widgetLgAmount">$50.00</td>
+                        <td className="widgetLgStatus"><Button type="Approved" /></td>
+                    </tr>
+                </tbody>
             </table>
         </div>
     )
