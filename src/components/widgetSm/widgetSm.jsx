da09912842f53f7a45bbba2da0becfe1e51c4ce4
Lucas <lucas2carlos@gmail.com>
2022-07-19 14:00:45 -0400

    building small widget

--- src/components/widgetSm/widgetSm.jsx
+++ src/components/widgetSm/widgetSm.jsx
@@ -7,7 +7,7 @@ export default function WidgetSm(props){
             <span className="widgetSmTitle">New Join Members</span>
             <ul className="widgetSmList">
                 <li className="widgetSmListItem">
-                    <img src="https://img.pokemondb.net/artwork/eevee.jpg" alt="" />
+                    <img src="https://img.pokemondb.net/artwork/pikachu.jpg" alt="" />
                     <div className="widgetSmUser">
                         <span className="widgetSmUsername">Ash Ketchum</span>
                         <span className="widgetSmUserTitle">Software Engineer</span>
@@ -18,9 +18,31 @@ export default function WidgetSm(props){
                     </button>
                 </li>
                 <li className="widgetSmListItem">
-                    <img src="https://img.pokemondb.net/artwork/eevee.jpg" alt="" />
+                    <img src="https://img.pokemondb.net/artwork/staryu.jpg" alt="" />
                     <div className="widgetSmUser">
-                        <span className="widgetSmUsername">Ash Ketchum</span>
+                        <span className="widgetSmUsername">Misty Aqua</span>
+                        <span className="widgetSmUserTitle">Software Engineer</span>
+                    </div>
+                    <button className="widgetSmButton">
+                        <MdVisibility />
+                        Display
+                    </button>
+                </li>
+                <li className="widgetSmListItem">
+                    <img src="https://img.pokemondb.net/artwork/onix.jpg" alt="" />
+                    <div className="widgetSmUser">
+                        <span className="widgetSmUsername">Brock Mason</span>
+                        <span className="widgetSmUserTitle">Software Engineer</span>
+                    </div>
+                    <button className="widgetSmButton">
+                        <MdVisibility />
+                        Display
+                    </button>
+                </li>
+                <li className="widgetSmListItem">
+                    <img src="https://img.pokemondb.net/artwork/meowth.jpg" alt="" />
+                    <div className="widgetSmUser">
+                        <span className="widgetSmUsername">Jessie Red</span>
                         <span className="widgetSmUserTitle">Software Engineer</span>
                     </div>
                     <button className="widgetSmButton">
@@ -31,29 +53,7 @@ export default function WidgetSm(props){
                 <li className="widgetSmListItem">
                     <img src="https://img.pokemondb.net/artwork/eevee.jpg" alt="" />
                     <div className="widgetSmUser">
-                        <span className="widgetSmUsername">Ash Ketchum</span>
-                        <span className="widgetSmUserTitle">Software Engineer</span>
-                    </div>
-                    <button className="widgetSmButton">
-                        <MdVisibility />
-                        Display
-                    </button>
-                </li>
-                <li className="widgetSmListItem">
-                    <img src="https://img.pokemondb.net/artwork/eevee.jpg" alt="" />
-                    <div className="widgetSmUser">
-                        <span className="widgetSmUsername">Ash Ketchum</span>
-                        <span className="widgetSmUserTitle">Software Engineer</span>
-                    </div>
-                    <button className="widgetSmButton">
-                        <MdVisibility />
-                        Display
-                    </button>
-                </li>
-                <li className="widgetSmListItem">
-                    <img src="https://img.pokemondb.net/artwork/eevee.jpg" alt="" />
-                    <div className="widgetSmUser">
-                        <span className="widgetSmUsername">Ash Ketchum</span>
+                        <span className="widgetSmUsername">Gary Blue</span>
                         <span className="widgetSmUserTitle">Software Engineer</span>
                     </div>
                     <button className="widgetSmButton">
