629e73f1820da1477bc66dc3832df6fbc0fd7e1d
Lucas <lucas2carlos@gmail.com>
2022-07-27 23:32:52 -0400

    adding user rows

--- src/dummyData.js
+++ src/dummyData.js
@@ -49,4 +49,16 @@ const userData = [
     },
   ];
 
+export const userRows = [
+  { id: 1, username: 'Jon', avatar: "https://img.pokemondb.net/sprites/trainers/heartgold-soulsilver/pryce.png", email: "jon@gmail.com", status: "active", transaction: "$120.00" },
+  { id: 2, username: 'Lannister', avatar: "https://img.pokemondb.net/sprites/trainers/ruby-sapphire/norman.png", email: "lannister@gmail.com", status: "active", transaction: "$430.00"},
+  { id: 3, username: 'Cheren', avatar: "https://img.pokemondb.net/sprites/trainers/black-white-2/cheren.png", email: "cheren@gmail.com", status: "active", transaction: "$210.00"},
+  { id: 4, username: 'Stark', avatar: "https://img.pokemondb.net/sprites/trainers/firered-leafgreen/giovanni.png", email: "stark@gmail.com", status: "active", transaction: "$1530.00"},
+  { id: 5, username: 'Targaryen', avatar: "https://img.pokemondb.net/sprites/trainers/black-white/clay.png", email: "targaryen@gmail.com", status: "active", transaction: "$120.00"},
+  { id: 6, username: 'Melisandre', avatar: "https://img.pokemondb.net/sprites/trainers/ruby-sapphire/glacia.png", email: "melisandre@gmail.com", status: "active", transaction: "$1270.00"},
+  { id: 7, username: 'Clifford', avatar: "https://img.pokemondb.net/sprites/trainers/diamond-pearl/flint.png", email: "clifford@gmail.com", status: "active", transaction: "$90.00"},
+  { id: 8, username: 'Frances', avatar: "https://img.pokemondb.net/sprites/trainers/firered-leafgreen/lorelei.png", email: "frances@gmail.com", status: "active", transaction: "$31420.00"},
+  { id: 9, username: 'Roxie', avatar: "https://img.pokemondb.net/sprites/trainers/diamond-pearl/candice.png", email: "roxie@gmail.com", status: "active", transaction: "$6120.00"},
+];
+
   export default userData
