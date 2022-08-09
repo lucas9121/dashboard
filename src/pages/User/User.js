91d8069b71130827ca8c698305fdcdb4737d1d6d
Lucas <lucas2carlos@gmail.com>
2022-08-09 17:28:51 -0400

    building single user page

--- src/pages/User/User.js
+++ src/pages/User/User.js
@@ -1,12 +1,15 @@
 import "./User.css"
-import {MdPermIdentity, MdCalendarToday, MdOutlinePhoneIphone, MdMailOutline, MdLocationSearching} from 'react-icons/md'
+import { Link } from "react-router-dom"
+import {MdPermIdentity, MdCalendarToday, MdOutlinePhoneIphone, MdMailOutline, MdLocationSearching, MdOutlinePublish} from 'react-icons/md'
 
 export default function User() {
     return(
         <div className="user">
             <div className="userTitleContainer">
                 <h1 className="userTitle">Edit User</h1>
-                <button className="userButton">Create</button>
+                <Link to="/newUser">
+                    <button className="userButton">Create</button>
+                </Link>
             </div>
             <div className="userContainer">
                 <div className="userShow">
@@ -42,7 +45,45 @@ export default function User() {
                         </div>
                     </div>
                 </div>
-                <div className="userUpdate"></div>
+                <div className="userUpdate">
+                    <span className="userUpdateTitle">Edit</span>
+                    <form className="userUpdateForm">
+                        <div className="userUpdateLeft">
+                            <div className="userUpdateItem">
+                                <label>Username</label>
+                                <input type="text" placeholder="pikalight99" />
+                            </div>
+                            <div className="userUpdateItem">
+                                <label>Username</label>
+                                <input type="text" placeholder="pikalight99" />
+                            </div>
+                            <div className="userUpdateItem">
+                                <label>Full Name</label>
+                                <input type="text" placeholder="Pikachu Lightning" />
+                            </div>
+                            <div className="userUpdateItem">
+                                <label>Email</label>
+                                <input type="text" placeholder="pikalight99@email.com" />
+                            </div>
+                            <div className="userUpdateItem">
+                                <label>Phone</label>
+                                <input type="text" placeholder="+1 123 456 7890" />
+                            </div>
+                            <div className="userUpdateItem">
+                                <label>Address</label>
+                                <input type="text" placeholder="New York | USA" />
+                            </div>
+                        </div>
+                        <div className="userUpdateRight">
+                            <div className="userUpdateUpload">
+                                <img src="https://img.pokemondb.net/artwork/pikachu.jpg" alt=""/>
+                                <label htmlFor="file"><MdOutlinePublish className="userUpdateIcon" /></label>
+                                <input type="file" id="file" style={{display: "none"}}/>
+                            </div>
+                            <button>Update</button>
+                        </div>
+                    </form>
+                </div>
             </div>
         </div>
     )
