ca1fe67454f00c1bbbb28b76229490ed85af6171
Lucas <lucas2carlos@gmail.com>
2022-08-10 15:14:53 -0400

    building new user page

--- src/pages/NewUser/NewUser.js
+++ src/pages/NewUser/NewUser.js
@@ -3,7 +3,51 @@ import "./NewUser.css"
 export default function NewUser() {
     return(
         <div className="newUser">
-            new user
+            <h1 className="newUserTitle">New User</h1>
+            <form className="newUserForm">
+                <div className="newUserItem">
+                    <label>Username</label>
+                    <input type="text" placeholder="steven" />
+                </div>
+                <div className="newUserItem">
+                    <label>Full Name</label>
+                    <input type="text" placeholder="Steven Ruby" />
+                </div>
+                <div className="newUserItem">
+                    <label>Email</label>
+                    <input type="text" placeholder="steven@email.com" />
+                </div>
+                <div className="newUserItem">
+                    <label>Password</label>
+                    <input type="password" placeholder="password" />
+                </div>
+                <div className="newUserItem">
+                    <label>Phone</label>
+                    <input type="text" placeholder="+1 123 987 6543" />
+                </div>
+                <div className="newUserItem">
+                    <label>Address</label>
+                    <input type="text" placeholder="New York | USA" />
+                </div>
+                <div className="newUserItem">
+                    <label>Gender</label>
+                    <div className="newUserGender">
+                        <input type="radio" name="gender" id="male" value="male" />
+                        <label for="male">Male</label>
+                        <input type="radio" name="gender" id="female" value="female" />
+                        <label for="female">Female</label>
+                        <input type="radio" name="gender" id="other" value="other" />
+                        <label for="other">Other</label>
+                    </div>
+                    <div className="newUserItem">
+                        <label>Active</label>
+                        <select className="newUserSelect" name="active" id="active">
+                            <option value="yes">Yes</option>
+                            <option value="no">No</option>
+                        </select>
+                    </div>
+                </div>
+            </form>
         </div>
     )
 }
